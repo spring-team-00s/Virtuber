118cfbb23eee50a29d32c196228ee0214dcc117d
박영균 <pyk09667@gmail.com>
2026-06-08 16:42:01 +0900

    first commit

---
+++ src/main/java/org/example/virtuber/VirtuberApplication.java
@@ -0,0 +1,13 @@
+package org.example.virtuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtuberApplication.class, args);
+    }
+
+}
