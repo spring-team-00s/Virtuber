bf4b7276f742a9aceb867abb1f456c6efb686097
studioym-bb <studioym.bb@gmail.com>
2026-06-19 13:20:31 +0900

    make repository _ account 빼고

--- src/main/java/org/example/virtuber/trade/repository/TradeRepository.java
+++ src/main/java/org/example/virtuber/trade/repository/TradeRepository.java
@@ -1,4 +1,13 @@
 package org.example.virtuber.trade.repository;
 
-public interface TradeRepository {
+import org.example.virtuber.trade.entity.Trade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TradeRepository extends JpaRepository<Trade, Long> {
+  // 계좌 거래내역 최신순 정렬
+  List<Trade> findAllByAccount_IdOrderByTradedTimeDesc(Long accountId);
+  // 한강버튼 거래내역 삭제
+  void deleteAllByAccount_Id(Long accountId);
 }
